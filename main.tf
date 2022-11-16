2440fbc78ee96fd787a57b005b63c8d7226d42ce
quadma <77735260+quadma@users.noreply.github.com>
2022-11-16 15:44:47 -0500

    fix

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "aws_security_group_rule" "blog_http_in"{
   to_port = 80
   protocol="tcp"
   cidr_blocks = ["0.0.0.0/0"]
-  security_group_id - aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_https_in"{
@@ -51,7 +51,7 @@ resource "aws_security_group_rule" "blog_https_in"{
   to_port = 443
   protocol="tcp"
   cidr_blocks = ["0.0.0.0/0"]
-  security_group_id - aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
 
 resource "aws_security_group_rule" "blog_everything_out"{
@@ -61,5 +61,5 @@ resource "aws_security_group_rule" "blog_everything_out"{
   protocol  ="-1"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id - aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 }
