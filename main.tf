c91a311b3971c33fe222dbbc51d2534c792e0047
quadma <77735260+quadma@users.noreply.github.com>
2022-11-16 15:37:48 -0500

    fix bug

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "aws_security_group_rule" "blog_http_in"{
   type = "ingress"
   from_port = 80
   to_port = 80
-  protocol="tcp'
+  protocol="tcp"
   cidr_blocks = {"0.0.0.0/0}
   security_group_id - aws_security_group.blog.id
 }
@@ -51,7 +51,7 @@ resource "aws_security_group_rule" "blog_https_in"{
   type = "ingress"
   from_port = 443
   to_port = 443
-  protocol="tcp'
+  protocol="tcp"
   cidr_blocks = {"0.0.0.0/0}
   security_group_id - aws_security_group.blog.id
 }
@@ -60,7 +60,7 @@ resource "aws_security_group_rule" "blog_everything_out"{
   type      = "engress"
   from_port = 0
   to_port   = 0
-  protocol  ="-1'
+  protocol  ="-1"
   cidr_blocks = {"0.0.0.0/0}
 
   security_group_id - aws_security_group.blog.id
