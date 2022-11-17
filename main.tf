39760d294d14209136f91a67522c5f9ca162eaef
quadma <77735260+quadma@users.noreply.github.com>
2022-11-17 14:13:19 -0500

    fix

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ module "blog_sg" {
   version = "4.16.1"
   name    = "blog"
 
-  vpc_id              = data.aws_vpc.default.id
+  vpc_id              = module.vpc.public_subnets
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules        = ["all-all"]
