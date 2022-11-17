afdf2732c7b533c18f799b8d65b10bbdced2eedd
quadma <77735260+quadma@users.noreply.github.com>
2022-11-17 15:23:11 -0500

    fix

--- main.tf
+++ main.tf
@@ -90,46 +90,9 @@ module "blog_sg" {
   version = "4.16.1"
   name    = "blog"
 
-  vpc_id              = module.blog_vpc.public_subnets[0]
+  vpc_id              = module.blog_vpc.vpc_id
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules        = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
-
-/*   replaced with above module
-resource "aws_security_group" "blog"{
-  name        = "blog"
-  description = "Allow http https in everything out"
-
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "blog_http_in"{
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    ="tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_https_in"{
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    ="tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_everything_out"{
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    ="-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-*/
