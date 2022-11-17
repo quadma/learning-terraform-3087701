d7c7953e7207744832a9c0ce129bd5782b9aa227
quadma <77735260+quadma@users.noreply.github.com>
2022-11-17 14:10:44 -0500

    module vpc

--- main.tf
+++ main.tf
@@ -14,9 +14,29 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+/*
 data "aws_vpc" "default"{
   default = true
 }
+*/
 
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
@@ -32,7 +52,7 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.16.1"
-  name    = "blog_new"
+  name    = "blog"
 
   vpc_id              = data.aws_vpc.default.id
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
