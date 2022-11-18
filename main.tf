3768de263d1890392b63c347e69c4bb32e2637ae
quadma <77735260+quadma@users.noreply.github.com>
2022-11-18 08:38:58 -0500

    autoscaling

--- main.tf
+++ main.tf
@@ -32,18 +32,23 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.3"
+  name    = "blog"
+  min_size = 1
+  max_size = 2
+  
+  vpc_zoneidentifier = module.blog_vpc.public_subnets
+  target_group_arns = module.bl.target_group_arns
+  security_groups = [module.blog_sg.security_group_id]
+  
+  image_id           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  tags = {
-    Name = "HelloWorld"
-  }
 }
 
-module "alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
@@ -63,8 +68,6 @@ module "alb" {
       target_type      = "instance"
       }
   ]
-
-
 
   http_tcp_listeners = [
     {
