1112c77f4eb765756384a49f1e624fe7224db973
quadma <77735260+quadma@users.noreply.github.com>
2022-11-18 09:47:44 -0500

    use variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,41 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "ami_filter"{
+  description = "Name filter and owner for ami string"
+
+  type = object({
+    name  = string
+    owner = string
+  })
+
+  default = {
+  name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+  owner = "979382823631" # Bitnami
+  }
+}
+
+variable "environment{
+  description "development environment"
+
+  type = object ({
+    name           = string
+    network_prefix = string  
+  })  
+
+  default = {
+    name = "dev"
+    network_prefix = "10.0"
+  }
+}
+
+variable "asg_min_size"{
+  description = "minimum number of instances in the asg"
+  default = 1
+}
+variable "asg_max_size"{
+  description = "minimum number of instances in the asg"
+  default = 2
+}
+  
